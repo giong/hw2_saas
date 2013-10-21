266142d0853262db27efbd5dcc1d11a77d6b9c21
lanstartupstanford <lan.startup.stanford@gmail.com>
2013-10-21 23:46:58 +0700

    draft hw 2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,42 @@
 class MoviesController < ApplicationController
 
   def show
+    @filter_params = {}
+    unless session[:filter_ratings].nil?
+      session[:filter_ratings].each do |f|
+       @filter_params[f] = 1
+      end
+    end
+    
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
 
   def index
+    @all_ratings = ['G', 'PG', 'PG-13', 'R']
+    @filter_ratings = params[:ratings].keys unless params[:ratings].nil?
     @movies = Movie.all
+    @sort = params[:sort]
+    
+    if @sort.nil?
+      @sort = session[:sort]
+    else
+      session[:sort] = @sort
+    end
+    
+    if @filter_ratings.nil?
+      @filter_ratings = session[:filter_ratings]
+    else
+      session[:filter_ratings] = @filter_ratings
+    end
+    if @sort == "title"
+      @movies = @movies.sort_by{ |m| m.title }
+    elsif @sort == "date" 
+      @movies = @movies.sort_by{ |m| m.release_date }
+    elsif params[:commit] == "Refresh" || !session[:filter_ratings].nil?
+      @movies = Movie.find_all_by_rating(@filter_ratings)
+    end
   end
 
   def new
